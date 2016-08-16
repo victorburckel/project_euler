00f4f4613d110bf21bbf1e254795520cd7ace2f1
victorburckel <victor.burckel@gmail.com>
2016-08-16 22:07:19 +0200

    problem23

--- Euler/main.cpp
+++ Euler/main.cpp
@@ -9,6 +9,6 @@ void problem5()
 void main()
 {
 #define PROBLEM( i ) std::cout << "problem" #i ": " << euler::problem ## i() << std::endl;
-	PROBLEM( 21 );
+	PROBLEM( 23 );
 #undef PROBLEM
 }
---
+++ Euler/problem23.cpp
@@ -0,0 +1,24 @@
+#include "euler.h"
+#include "divisors.h"
+#include <boost/unordered_set.hpp>
+
+size_t euler::problem23()
+{
+	const auto is_abundant = []( size_t n ) {
+		return euler::getSumOfProperDivisorsAlt( n ) > n;
+	};
+
+	const auto abundants = boost::irange( 12_u, 28123_u ) | boost::adaptors::filtered( is_abundant ) | to_vector;
+	assert( abundants[ 0 ] == 12_u );
+
+	const auto is_sum_of_two_abundants = [ &abundants, abundants_checker = boost::unordered_set< size_t >( abundants.begin(), abundants.end() ) ]( size_t n ) {
+		for( auto it = abundants.begin(); it != abundants.end() && *it < n; ++it )
+			if( abundants_checker.find( n - *it ) != abundants_checker.end() )
+				return true;
+
+		return false;
+	};
+	
+	const auto result = boost::irange( 1_u, 28123_u ) | boost::adaptors::filtered( [ is_sum_of_two_abundants ]( size_t n ) { return !is_sum_of_two_abundants( n ); } ) | to_vector;
+	return boost::accumulate( result, 0_u );
+}
